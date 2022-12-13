a8e1d40a781fcc79f368ae75cae361822492aabb
Harrison Bishop <harrisonbishsp@gmail.com>
2022-12-13 12:45:11 +0000

    created the Gameboard module, created the board with null strings and used a nested loop to iterate over each array item. The loop gives each item an id and appends it to the html container, with a 1px black border

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const name = "adam";
-
-const lalal = "laoaoa";
+//
+const Gameboard = (() => {
+  const gameBoard = document.getElementById("game-board");
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      squaresId = "square-" + i + "-" + j;
+      const div = document.createElement("div");
+      div.id = squaresId;
+      div.style.border = "1px solid black"
+      gameBoard.appendChild(div)
+      console.log(div.id);
+    }
+  }
+})();
