c52c824b364b91b85233f185c5e5118605acf75b
Harrison Bishop <harrisonbishsp@gmail.com>
2022-12-13 13:34:22 +0000

    added a classname to the squares to style them

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-//
+// Module for the gameboard
 const Gameboard = (() => {
   const gameBoard = document.getElementById("game-board");
   const board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+    ["", "", "O"],
+    ["", "O", ""],
+    ["X", "", ""],
   ];
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      squaresId = `square-${i}-${j}`
+      // squaresId = `square-${i}-${j}`;
       const div = document.createElement("div");
-      div.id = squaresId;
+      div.className = "boardSquare";
       div.style.border = "1px solid black";
+      div.innerText = board[i][j];
       gameBoard.appendChild(div);
       console.log(div.id);
     }
   }
+  gameBoard.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
 })();
 
 // factory function for player Name
