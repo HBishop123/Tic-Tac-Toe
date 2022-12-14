66f5e6b1ad2c1b15a05db739e5fa6f2c5cc6fa1d
Harrison Bishop <harrisonbishsp@gmail.com>
2022-12-14 12:27:04 +0000

    made some changes to the objects, inlcuding adding a click event to add symbols to the board, and then reverse the symbol selected each go

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 // Module for the gameboard
-const Gameboard = (() => {
+const CreateGameBoard = (() => {
   const gameBoard = document.getElementById("game-board");
   const board = [
-    ["", "", "O"],
-    ["", "O", ""],
-    ["X", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      // squaresId = `square-${i}-${j}`;
+      squaresId = `square-${i}-${j}`;
       const div = document.createElement("div");
+      div.id = squaresId;
       div.className = "boardSquare";
       div.style.border = "1px solid black";
       div.innerText = board[i][j];
       gameBoard.appendChild(div);
-      console.log(div.id);
     }
   }
-  gameBoard.addEventListener("click", (e) => {
-    console.log(e.target);
-  });
 })();
 
-// factory function for player Name
 function Player(name, symbol) {
   return {
     name,
@@ -30,5 +26,29 @@ function Player(name, symbol) {
   };
 }
 
-const player1 = Player("Player 1", "X");
-const player2 = Player("Player 2", "O");
+const Gameflow = (() => {
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
+
+  let currentTurn = player1.symbol;
+  currentTurn = player1 ? player1.symbol : player2.symbol;
+
+  const eachSquare = document.querySelectorAll(".boardSquare");
+
+  eachSquare.forEach((square) => {
+    square.addEventListener("click", clickEvent, { once: true });
+
+    function clickEvent() {
+      square.innerText = currentTurn;
+      changeTurn()
+    }
+  });
+
+  function changeTurn() {
+    if (currentTurn === player1.symbol) {
+      currentTurn = player2.symbol;
+    } else if (currentTurn === player2.symbol) {
+      currentTurn = player1.symbol;
+    }
+  }
+})();
